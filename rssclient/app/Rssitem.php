51581bce9ef60bfe5bbea3363bec8d88908e12a8
Victor <victor.rengifo@gmail.com>
2019-11-27 12:36:34 -0600

    updating Rssitem.php to use Filterable

--- rssclient/app/Rssitem.php
+++ rssclient/app/Rssitem.php
@@ -4,9 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * use of Filterable Trait
+ */
+use App\Filters\Filterable;
+
 class Rssitem extends Model
 {
-    
+    /**
+     * enable the use of Filterable trait
+     */
+    use Filterable;
+        
     /**
      * attributes available to fill
      *
